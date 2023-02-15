b683fae6b3e901b83c025407bee3a5d137ef3203
Sebastian Ramirez <71026252+SebasRamz-Murillo@users.noreply.github.com>
2023-02-15 11:45:43 -0600

    LoginYaCasi

--- PracticaAngular/src/app/login/login.component.ts
+++ PracticaAngular/src/app/login/login.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  onSubmit() {
+    const username = (document.getElementById('username') as HTMLInputElement).value;
+    const password = (document.getElementById('password') as HTMLInputElement).value;
 
+    // Envía los datos del formulario al servidor
+    console.log('Nombre de usuario:', username);
+    console.log('Contraseña:', password);
+  }
 }
