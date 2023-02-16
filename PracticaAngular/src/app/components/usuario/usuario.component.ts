15c1dfb9ec40884c9a16e014f1480091f45e38e2
Abelardo <abelardoreyes256@gmail.com>
2023-02-16 17:32:33 -0600

    Se agrego CSS de Tailwind y se crearon componentes

---
+++ PracticaAngular/src/app/components/usuario/usuario.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { Route,ActivatedRoute, ParamMap } from '@angular/router';
+
+@Component({
+  selector: 'app-usuario',
+  templateUrl: './usuario.component.html',
+  styleUrls: ['./usuario.component.css']
+})
+export class UsuarioComponent {
+
+  constructor(){
+
+  }
+
+  ngOnInit(): void{
+  }
+
+}
