f7fdce46a723168e76ef14cbe68454c9759d43ec
Sebastian Ramirez <71026252+SebasRamz-Murillo@users.noreply.github.com>
2023-02-20 15:57:08 -0600

    inicio zona-objetos

--- PracticaAngular/src/app/components/mh/zona-objeto/zona-objeto.component.ts
+++ PracticaAngular/src/app/components/mh/zona-objeto/zona-objeto.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
+import { Objeto } from 'src/app/Models/objeto.model';
+import { ObjetoService } from 'src/app/services/objeto.service';
+import { OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
 
 @Component({
-  selector: 'app-zona-objeto',
+  selector: 'app-objeto',
   templateUrl: './zona-objeto.component.html',
   styleUrls: ['./zona-objeto.component.css']
 })
-export class ZonaObjetoComponent {
+export class ZonaObjetoComponent implements OnInit, OnDestroy {
+  objetos: Objeto[] = [];
+  suscription?:Subscription;
 
+  constructor(private objetoService: ObjetoService) { }
+  ngOnInit(): void {
+    this.getObjetos();
+    this.suscription = this.objetoService.get_refresh$().subscribe(() => {
+      this.getObjetos();
+    }
+    );
+  }
+  ngOnDestroy():void {
+    this.suscription?.unsubscribe();
+    console.log('Se destruyó el componente');
+  }
+  getObjetos() {
+    this.objetoService.getIObjeto().subscribe(data => this.objetos = data);
+  }
 }
