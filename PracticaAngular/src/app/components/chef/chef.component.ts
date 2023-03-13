816e97e332ea65fdc89eee82d0d028b6b044aec6
Abelardo <abelardoreyes256@gmail.com>
2023-03-13 13:37:28 -0600

    polling en controlador chef

--- PracticaAngular/src/app/components/chef/chef.component.ts
+++ PracticaAngular/src/app/components/chef/chef.component.ts
@@ -9,6 +9,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Usuario } from 'src/app/models/usuario.model';
 import { environment } from 'src/environments/environment';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-chef',
@@ -32,12 +33,19 @@ export class ChefComponent implements OnInit {
     const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
 
     this.http.get<Usuario>(environment.URL_API+'/usuario/infoObjeto', { headers }).subscribe(data => this.usuario = data);
-    this.getChefs();
+    this.suscription = interval(10000).subscribe(() => {
+      this.getChefs();
+    });
     this.suscription = this.chefService.get_refresh$().subscribe(() => {
       this.getChefs();
 
     }
     );
+  }
+  ngOnDestroy() {
+    if (this.suscription) {
+      this.suscription.unsubscribe();
+    }
   }
 
   getChefs() {
--- PracticaAngular/src/environments/environment.ts
+++ PracticaAngular/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  URL_API: 'http://192.168.120.87:3333',
+  URL_API: 'http://127.0.0.1:3333',
   URL_SIGNED: '',
   usuarioInactivo: false,
 };
