339e650b5257f934b188990e36a4aa70d8b42b0e
Abelardo <abelardoreyes256@gmail.com>
2023-03-13 21:43:15 -0600

    Revert "EventSource"
    This reverts commit 9f7f5bae3f63b5a577f78e14a9ab25127e1a81ce.

--- PracticaAngular/src/app/components/ingrediente/ingrediente.component.ts
+++ PracticaAngular/src/app/components/ingrediente/ingrediente.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component } from '@angular/core';
+import { OnInit, OnDestroy } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { Ingrediente } from 'src/app/models/ingrediente.model';
 import { IngredienteService } from 'src/app/services/ingrediente.service';
@@ -12,63 +13,27 @@ import { Usuario } from 'src/app/models/usuario.model';
   templateUrl: './ingrediente.component.html',
   styleUrls: ['./ingrediente.component.css']
 })
-export class IngredienteComponent implements OnInit, OnDestroy {
+export class IngredienteComponent implements OnInit {
   ingrediente: Ingrediente[] = [];
   suscription?: Subscription;
   usuario?: Usuario;
   myToken = localStorage.getItem('token') || '';
-  eventSource?: EventSource;
 
-  constructor(
-    private ingredienteService: IngredienteService,
+  constructor(private ingredienteService: IngredienteService,
     private loginService: LoginService,
-    private http: HttpClient
-  ) {}
-
+    private http: HttpClient) { }
   ngOnInit(): void {
     const token = this.myToken;
     const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
-    this.http
-      .get<Usuario>(`${environment.URL_API}/usuario/infoObjeto`, { headers })
-      .subscribe((data) => (this.usuario = data));
-
+    this.http.get<Usuario>(environment.URL_API+'/usuario/infoObjeto', { headers }).subscribe(data => this.usuario = data);
     this.getIngredientes();
-
-    // Crea el objeto EventSource
-    this.eventSource = new EventSource(`${environment.URL_API}/ingredientes/stream`);
-
-    // Suscríbete al evento "message"
-    this.eventSource.addEventListener('message', (event: MessageEvent) => {
-      // Actualiza la lista de ingredientes
-      this.getIngredientes();
-    });
-
-    // Maneja el evento "error"
-    this.eventSource.addEventListener('error', (event: Event) => {
-      console.error('EventSource error:', event);
-      // Intenta reconectar después de 5 segundos
-      setTimeout(() => this.ngOnInit(), 5000);
-    });
-
-    // Suscríbete al evento de actualización de ingredientes
     this.suscription = this.ingredienteService.get_refresh$().subscribe(() => {
       this.getIngredientes();
     });
   }
 
-  ngOnDestroy(): void {
-    // Cierra la conexión con EventSource
-    if (this.eventSource) {
-      this.eventSource.close();
-    }
-    // Cancela la suscripción al servicio de ingredientes
-    if (this.suscription) {
-      this.suscription.unsubscribe();
-    }
-  }
-
-  // Métodos
+  //Metodos
   getIngredientes() {
-    this.ingredienteService.getIngredientes().subscribe((data) => (this.ingrediente = data));
+    this.ingredienteService.getIngredientes().subscribe(data => this.ingrediente = data);
   }
 }
