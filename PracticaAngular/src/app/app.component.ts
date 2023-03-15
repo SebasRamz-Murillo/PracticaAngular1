dce0739f4e24187e8ec27bea4c25106cfd0177df
Abelardo <abelardoreyes256@gmail.com>
2023-03-15 13:40:40 -0600

    Revert "EventSource pIrata"
    This reverts commit bfaf484d27d1320943ec3511b6326b8eebdcd7da.

--- PracticaAngular/src/app/app.component.ts
+++ PracticaAngular/src/app/app.component.ts
@@ -1,34 +1,10 @@
 import { Component } from '@angular/core';
-import { OnInit, ChangeDetectorRef } from '@angular/core';
-import { socket } from 'src/environments/socket';
-import { Objeto } from 'src/app/models/objeto.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-  objeto = { nombre: '', descripcion: '', rareza: '', valor: 1, limiteBolsa: 1, limiteInventario: 1 }
-  cambio = false;
-
-
+export class AppComponent {
   title = 'PracticaAngular';
-  constructor(private cd: ChangeDetectorRef) { }
-  ngOnInit(): void {
-    this.emitirEvento();
-  }
-
-  emitirEvento() {
-    socket.on("connect", () => {
-      console.log(socket.id); // x8WIv7-mJelg7on_ALbx
-    });
-    socket.on('eliminarObjeto', (objetos) => {
-      console.log(objetos)
-      this.cambio = true;
-      this.objeto = objetos;
-      this.cd.detectChanges();
-      // manejar los objetos recibidos aquí
-    })
-  }
 }
